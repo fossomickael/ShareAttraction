a42d9c74250701486f2d7ef4ac67f6c503744ae9
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-25 17:08:05 +0200

    new feature: user can see attractions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :attractions, only: [:create, :new, :show]
+  resources :attractions, only: [:create, :new, :show, :index]
   
   resources :posts, only: [:create, :new, :show]
 
