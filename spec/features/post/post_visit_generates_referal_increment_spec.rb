743cbfb430cae6871263636e777c9cdf1df3b0b1
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-26 22:55:45 +0200

    increment

---
+++ spec/features/post/post_visit_generates_referal_increment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "when post is visited" do
+  scenario "increments the referal" do
+    users = FactoryBot.create_list(:user, 2)
+    post_author = users[1]
+    other_user = users[0]
+    attraction = create(:attraction)
+    post = create(:post)
+
+    visit "#{post_path(post)}?ref=#{other_user.referrer_code}"
+    create(:attraction_member, user: post_author, attraction: attraction)
+    create(:attraction_member, user: other_user, attraction: attraction)
+
+    referrer = PostReferrer.where(user: other_user, post: post).take
+    expect(referrer.count == 1).to be true
+  end
+end
