298d90189f87e08e3b70be0098184254e9db53cd
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-27 20:25:50 +0200

    article homepage first design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @posts = Post.all
+    @posts = Post.includes(:user, :attraction)
     @home = true
   end
 end
