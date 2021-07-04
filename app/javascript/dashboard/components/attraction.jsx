9a1e3e3b00ff78e647037bb948cc3fc7978261a5
Mickael FOSSO <fosso.mickael@gmail.com>
2021-07-04 17:48:17 +0200

    responsive notifications

--- app/javascript/dashboard/components/attraction.jsx
+++ app/javascript/dashboard/components/attraction.jsx
@@ -4,9 +4,11 @@ import Post  from "./post";
 const Attraction = (props) => {
   return ( <div className="attraction">
                <h2>{props.attraction.name}</h2>
+               <div className="row">
                  {props.attraction.posts.map((post) => {
                    return <Post key={post.id} post={post} />;
                  })}
+                </div>
             </div>
   );
 };
