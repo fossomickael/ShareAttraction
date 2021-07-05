23aa715b85c6d659fe1e3c80a387e4892dfbe631
Mickael FOSSO <fosso.mickael@gmail.com>
2021-07-05 22:02:38 +0200

    mailer setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,15 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'shareattraction.heroku.com',
+  :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
