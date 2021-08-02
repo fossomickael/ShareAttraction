f8aa07197d993f871ae0ba070380c103f360c91b
Mickael FOSSO <fosso.mickael@gmail.com>
2021-08-02 12:56:56 +0200

    javascript get stats with api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::PostsController < ActionController::API
   def referrers
     @post = Post.find(params[:id])
-    @referrers = PostReferrer.includes(:user).where(post_id: params[:id])
-    
+    @referrers = PostReferrer.includes(:user).where(post_id: params[:id]) 
   end
 end
--- app/javascript/plugins/init_post_chart.js
+++ app/javascript/plugins/init_post_chart.js
@@ -5,6 +5,23 @@ const initPostChart = () => {
     const ctx = document.getElementById('PostChart');
     if (!ctx)
     return
+    const postid = ctx.dataset.postid;
+    fetchStats(postid).then(content => console.log(content)) 
+
+
+}
+
+const  fetchStats = async (postid) => {
+    try {
+      const url = `/api/v1/posts/${postid}/referrers`
+      const response = await fetch(url);
+      const stats = await response.json();
+      return stats;
+    }
+    catch (error) {
+      console.log(error)
+    }
+     
 }
 
 export {initPostChart}
