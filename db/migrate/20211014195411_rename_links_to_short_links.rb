1eee9114c6f27192622d97c871346f7a1fe05935
Mickael FOSSO <fosso.mickael@gmail.com>
2021-10-19 23:20:00 +0200

    new migrations because of heroku error

--- db/migrate/20211014195411_rename_links_to_short_links.rb
+++
@@ -1,9 +0,0 @@
-class RenameLinksToShortLinks < ActiveRecord::Migration[6.1]
-  def self.up
-    rename_table :links, :short_links
-  end
-
-  def self.down
-    rename_table :short_links, :links
-  end
-end
