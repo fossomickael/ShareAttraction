1bf66c3954a50162f94c6eaab0ce0f3a9b75f7ca
Mickael FOSSO <fosso.mickael@gmail.com>
2021-12-08 10:44:48 +0100

    fix test

--- spec/features/user/user_creates_attraction_spec.rb
+++ spec/features/user/user_creates_attraction_spec.rb
@@ -7,7 +7,7 @@ feature "user creates attraction" do
     fill_in "attraction_name", with: "growaudience"
     fill_in "attraction_description", with: "This attraction group is to grow your audience"
     click_on "Create Attraction"
-    expect(page).to have_css 'h1', text: 'growaudience'
+    expect(page).to have_css 'h1', text: 'Invite members to your attraction'
   end
 
   scenario "and his member of the new attraction" do
