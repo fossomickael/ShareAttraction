7efe5e7bd2a8264bcf0629c36015b73a92a37efd
Mickael FOSSO <fosso.mickael@gmail.com>
2021-12-29 21:20:08 +0100

    add translations

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module ShareAttraction
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :'fr'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
